0cac463f0df82ce658dadae2424431779becbe59
Oliv-19 <betza.bruguera@gmail.com>
2025-11-27 15:58:20 -0400

    Add exercise 1.3 and changed readme file

--- Part1/courseInfo/src/App.jsx
+++ Part1/courseInfo/src/App.jsx
@@ -2,43 +2,49 @@ function Header({courseName}){
   return <h1>{courseName}</h1>
 }
 
-function Part({part, exercises}){
+function Part({part}){
   return (
     <p>
-      {part} {exercises}
+      {part.name} {part.exercises}
     </p>
   )
 }
 
-function Content({part1, part2, part3, exercises1, exercises2, exercises3}){
+function Content({part1, part2, part3}){
   return (
     <>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
+      <Part part={part1}/>
+      <Part part={part2}/>
+      <Part part={part3}/>
     </>
   )
 }
 
-function Total({total}){
+function Total({exercises1, exercises2, exercises3}){
   return (
-    <p>Number of exercises {total} </p>
+    <p>Number of exercises {exercises1 + exercises2 + exercises3} </p>
   )
 }
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+   const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header courseName={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2}  part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
