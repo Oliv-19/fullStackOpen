07111655047d53b1ef0c8370536eb690285554f2
Oliv-19 <betza.bruguera@gmail.com>
2025-12-17 09:03:02 -0400

    Add exercise 3.20

--- Part3/Phonebook_backend/models/person.js
+++ Part3/Phonebook_backend/models/person.js
@@ -11,8 +11,19 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
+    required: true
   },
-  number: String,
+  number: {
+    type:String,
+    minLength:8,
+    validate: {
+      validator: function(val){
+        return /\d{2,3}-\d{7,8}/.test(val)
+      },
+      message: props=> `${props.value} is not a valid phone number`
+    },
+    required:true
+  },
 })
 personSchema.set('validateBeforeSave', true)
 personSchema.set('toJSON', {
