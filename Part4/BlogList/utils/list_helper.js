ac30e51e8e018598eab2b39c94e493df809eef89
Oliv-19 <betza.bruguera@gmail.com>
2026-01-01 09:36:21 -0400

    Add exercise 4.6

--- Part4/BlogList/utils/list_helper.js
+++ Part4/BlogList/utils/list_helper.js
@@ -12,8 +12,32 @@ const favoriteBlog = (blogs) => {
     },blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+    if(blogs.length == 0){
+        return {}
+    }
+    const authors = {}
+    blogs.forEach(blog => {
+        if(Object.keys(authors).includes(blog.author)){
+            authors[blog.author] = authors[blog.author]+1
+        }else{
+            authors[blog.author] = 1
+        }
+    });
+    const most =  Object.entries(authors).reduce((prev , curr)=> {
+       prev =  prev[1] < curr[1] ? curr : prev
+       return prev
+    },[Object.keys(authors).at(0), Object.values(authors).at(0)])
+
+    return {
+        author: most[0],
+        blogs: most[1]
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
