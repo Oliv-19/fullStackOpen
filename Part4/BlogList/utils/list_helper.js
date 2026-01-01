8c860eb8aec38f139b28c03cba01e06e6fa8e718
Oliv-19 <betza.bruguera@gmail.com>
2026-01-01 09:03:05 -0400

    Add exercise 4.5

--- Part4/BlogList/utils/list_helper.js
+++ Part4/BlogList/utils/list_helper.js
@@ -5,7 +5,15 @@ const totalLikes = (blogs) => {
     return blogs.length === 0? 0:blogs.reduce((prev, curr)=> prev + curr.likes,0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.length === 0? {} : blogs.reduce((prev, curr)=> {
+       prev =  prev.likes < curr.likes? curr : prev
+       return prev
+    },blogs[0])
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
