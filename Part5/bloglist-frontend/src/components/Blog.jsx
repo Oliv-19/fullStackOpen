81c060a9e5f6495f1675096b8471b96f99c79bfb
Oliv-19 <betza.bruguera@gmail.com>
2026-01-18 08:18:09 -0400

    Add exercise 5.7

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const isVisible = () =>{
+    setIsOpen(!isOpen)
+  }
+  let styles = {
+    border: '1px solid black',
+    width: 'fit-content',
+    padding:'5px',
+    margin: '5'
+  }
+  return (
+    <div style={styles }>
+    {isOpen? (
+        <>
+          <p>{blog.title} <button onClick={isVisible}>hide</button></p> 
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.author}</p>
+        </>
+      ): (
+        <>
+          {blog.title} {blog.author} <button onClick={isVisible}>view</button>
+        </>    
+    )}
+    </div>
+
+  )
+}
 
 export default Blog
