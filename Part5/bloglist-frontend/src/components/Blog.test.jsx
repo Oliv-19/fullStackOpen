3df8359c6b6dd6f6021ba624b8b8a10fc31d36f0
Oliv-19 <betza.bruguera@gmail.com>
2026-01-22 17:14:53 -0400

    Add exercise 5.16

--- Part5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { userEvent} from '@testing-library/user-event'
 import Blog from './Blog'
+import NewBlogForm from './NewBlogForm'
 
 describe('<Blogs />', () => {
     test('displays Title and author by default', () => {
@@ -54,5 +55,27 @@ describe('<Blogs />', () => {
         await user.click(likeBlogBtn)
         await user.click(likeBlogBtn)
         expect(mockFn.mock.calls).toHaveLength(2)
+    })
+    test('form calls createNewBlog with right details', async() => {
+        const user = userEvent.setup()
+        const mockFn = vi.fn()
+        const mockBlog = {
+            title:'Test Blog',
+            author: 'Test Author',
+            url: 'Test Url'
+        }
+        render(<NewBlogForm createNewBlog={mockFn}/>)
+        const titleInput = screen.getByLabelText('title:')
+        const authorInput = screen.getByLabelText('author:')
+        const urlInput = screen.getByLabelText('url:')
+        const submitBtn = screen.getByRole('button')
+
+        await user.type(titleInput, mockBlog.title)
+        await user.type(authorInput, mockBlog.author)
+        await user.type(urlInput, mockBlog.url)
+        await user.click(submitBtn)
+
+        expect(mockFn.mock.calls).toHaveLength(1)
+        expect(mockFn.mock.calls[0][0]).toEqual(mockBlog)
     })
 })
