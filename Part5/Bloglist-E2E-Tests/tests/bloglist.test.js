d9aba66822f9ea2265216ed7f26a35631aaf31d9
Oliv-19 <betza.bruguera@gmail.com>
2026-01-25 14:16:05 -0400

    Add exercise 5.19

--- Part5/Bloglist-E2E-Tests/tests/bloglist.test.js
+++ Part5/Bloglist-E2E-Tests/tests/bloglist.test.js
@@ -26,5 +26,23 @@ describe('Blog app', () => {
       await page.getByRole('button', {name: 'log in'}).click()
       await expect(page.getByText('root logged in')).not.toBeVisible()
     })
+    
+  })
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByLabel('Username').fill('root')
+      await page.getByLabel('Password').fill('sekret')
+      await page.getByRole('button', {name: 'log in'}).click()
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByText('Create new blog').click()
+      await page.getByLabel('title:').fill('test blog')
+      await page.getByLabel('author:').fill('root')
+      await page.getByLabel('url:').fill('none')
+      await page.getByRole('button', {name: 'create'}).click()
+
+      await expect(page.getByText('test blog root')).toBeVisible()
+    })
   })
 })
