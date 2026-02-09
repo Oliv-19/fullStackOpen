24d13c9fee57097988973d554e9b61ec51961053
Oliv-19 <betza.bruguera@gmail.com>
2026-02-09 12:02:01 -0400

    Add exercise 6.14

--- Part6/redux-anecdotes/src/App.jsx
+++ Part6/redux-anecdotes/src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react'
 import { AnecdoteForm } from './components/AnecdoteForm'
 import { AnecdoteList } from './components/AnecdoteList'
 import { Filter } from './components/Filter'
+import anecdotesService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(()=> {
+    anecdotesService.getAll().then((a) => dispatch(setAnecdotes(a)))
+  }, [dispatch])
   return (
     <div>
       <h2>Anecdotes</h2>
