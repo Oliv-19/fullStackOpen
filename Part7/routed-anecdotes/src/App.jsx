05b188a996333dc3012977d6ca0cdf23ac956fd9
Oliv-19 <betza.bruguera@gmail.com>
2026-02-26 11:21:17 -0400

    Add exercise 7.4

--- Part7/routed-anecdotes/src/App.jsx
+++ Part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Routes, Route, Link, useParams, redirect, Navigate} from 'react-router-dom'
+import { useField } from './hooks'
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -49,17 +50,16 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
@@ -70,15 +70,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...info}/>
         </div>
         <button>create</button>
       </form>
@@ -93,7 +93,7 @@ const Anecdote = ({anecdotes}) => {
 
   return (
     <div>
-      <h2>{anecdote.content}</h2>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
       <p>{`has ${anecdote.votes} votes`}</p>
       <p>for more info see <Link to={anecdote.info}>{anecdote.info}</Link></p>
     </div>
